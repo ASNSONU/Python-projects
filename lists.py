df3a0d2efea0efaeeef57c2965eac983ac6b70a9
ASNSONU <asnsonu066@gmail.com>
2026-04-01 09:35:38 +0530

    change wrone spalling

--- lists.py
+++ lists.py
@@ -1,5 +1,5 @@
-list1=["namne","manu","nikhil",",manu"]# list is mutable, so you can add , remove or modify emenets.
-tuple2=("ayush","priya","riya","sumit")# tuple is immutable so you can not change ,add or remove emenets.
+list1=["namne","manu","nikhil",",manu"]# list is mutable, so you can add , remove or modify elemenets.
+tuple2=("ayush","priya","riya","sumit")# tuple is immutable so you can not change ,add or remove elemenets.
 set3={"nikhil","rohit","karan","pawan"} # set is mutable it is   unordered(No index) and does not allow duplicate values.
 dict4={"name":"sonu","age":21,"city":"dehli"}#dictionary is mutable. it stores data in key- value pairs and kays must de unique.
 print(type(list1))
