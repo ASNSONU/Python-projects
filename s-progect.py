d827c79e2d79bcad97b892657ce43acbd0be23b0
ASNSONU <asnsonu066@gmail.com>
2026-04-01 09:54:45 +0530

    login form

---
+++ s-progect.py
@@ -0,0 +1,6 @@
+username=input("Enter your name : ").strip()
+userpassword=input("Enter user password : ").strip()
+if username=="asnsonu" and userpassword=="12345":
+    print("login successful  ")
+else:
+    print("login fail")
