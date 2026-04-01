cabb585e989486fc166c6957dc25279af3097e20
ASNSONU <asnsonu066@gmail.com>
2026-04-01 09:25:19 +0530

    add a new python file related list, tuple , set , disc data type

---
+++ lists.py
@@ -0,0 +1,8 @@
+list1=["namne","manu","nikhil",",manu"]# list is mutable, so you can add , remove or modify emenets.
+tuple2=("ayush","priya","riya","sumit")# tuple is immutable so you can not change ,add or remove emenets.
+set3={"nikhil","rohit","karan","pawan"} # set is mutable it is   unordered(No index) and does not allow duplicate values.
+dict4={"name":"sonu","age":21,"city":"dehli"}#dictionary is mutable. it stores data in key- value pairs and kays must de unique.
+print(type(list1))
+print(type(tuple2))
+print(type(set3)) 
+print(type(dict4))
